d0d476b47a8de99f0aa620fe2d7a54e2faa89b4d
minhthang9a4 <79731131+minhthang9a4@users.noreply.github.com>
2021-03-21 15:04:40 +0700

    Update chuoi dao nguoc.py

--- chuoi dao nguoc.py
+++ chuoi dao nguoc.py
@@ -1,7 +1,7 @@
 m=int(input("Nhap so phan tu: "))
 b=[]
 for j in range(0,m):
-    y=int(input("Nhap phan tu thu %d= " %(j)))
+    y=int(input("Nhap phan tu thu %d: " %(j)))
     b.append(y)
 print("chuoi da nhap la: ")
 print(b)
